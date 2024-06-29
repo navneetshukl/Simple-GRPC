542cd94be19b71536d8f0c1665ae046770314ebc
navneetshukl <navneetshukla824@gmail.com>
2024-06-29 14:31:26 +0530

    Simple grpc server and client

---
+++ greet/server/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"grpc/greet/proto"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+var addr string = "0.0.0.0:50051"
+
+type Server struct {
+	proto.GreetServiceServer
+}
+
+func main() {
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("failed to listen: %v\n", err)
+	}
+
+	log.Printf("Listening on %s\n", addr)
+
+	s := grpc.NewServer()
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v\n", err)
+	}
+}
